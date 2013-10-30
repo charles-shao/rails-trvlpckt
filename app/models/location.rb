7fdda67274ab8138f101c4cca1ac567bc73b6ede
charles <charles@intersect.org.au>
2013-10-30 17:22:44 +1100

    fixes

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,7 +1,7 @@
 class Location < ActiveRecord::Base
   attr_accessible :img_url, :lat, :lng
-  has_many :comments, :dependent => :destroy
-  has_many :photos, :dependent => :destroy
+  has_many :comment, :dependent => :destroy
+  has_many :photo, :dependent => :destroy
 
   accepts_nested_attributes_for :comment
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131024003941) do
+ActiveRecord::Schema.define(:version => 20131030055141) do
 
   create_table "comments", :force => true do |t|
     t.text     "message"
@@ -27,6 +27,14 @@ ActiveRecord::Schema.define(:version => 20131024003941) do
     t.string   "lng"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "username"
+  end
+
+  create_table "photos", :force => true do |t|
+    t.string   "filepath"
+    t.integer  "location_id"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
 end
